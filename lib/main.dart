10969bb87453600ea740cbf10f4093667665f1c1
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-06-29 13:36:06 +0200

    Organize files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kebab_katowice/app/home/home_page.dart';
+import 'package:kebab_katowice/app/login/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -38,18 +40,10 @@ class RootPage extends StatelessWidget {
         builder: (context, snapshot) {
           final user = snapshot.data;
           if (user == null) {
-            return Scaffold(
-              body: Center(
-                child: Text('jesteś niezalogowany'),
-              ),
-            );
+            return const LoginPage();
           }
 
-          return Scaffold(
-            body: Center(
-              child: Text('Jesteś zalogowany jako ${user.email}'),
-            ),
-          );
+          return HomePage(user: user);
         });
   }
 }
