a474572f7d5bd5552e6309ec876e3dc3db2b64ec
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-06-29 17:11:31 +0200

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var kebabName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('dwa'),
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'podaj nazwę restauracji',
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  restaurantName = newValue;
+                });
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'podaj nazwę kebaba',
+              ),
+              onChanged: (newValue) {
+                setState(() {
+                  kebabName = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: restaurantName.isEmpty || kebabName.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('restaurants').add({
+                        'name': restaurantName,
+                        'kebab': kebabName,
+                        'rating': 3.0
+                      });
+                    },
+              child: const Text('Dodaj'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
