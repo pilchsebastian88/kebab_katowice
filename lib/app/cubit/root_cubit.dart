3f3536a53ac677baf01fe1d304d9c2e48b4ce9e8
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-07-08 14:18:36 +0200

    Implement Root Cubit

---
+++ lib/app/cubit/root_cubit.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'root_state.dart';
+
+class RootCubit extends Cubit<RootState> {
+  RootCubit()
+      : super(const RootState(
+          user: null,
+          errorMessage: '',
+          isLoading: false,
+        ));
+
+  StreamSubscription? _streamSubscription;
+
+  Future<void> signOut() async {
+    FirebaseAuth.instance.signOut();
+  }
+
+  Future<void> start() async {
+    emit(
+      const RootState(
+        user: null,
+        errorMessage: '',
+        isLoading: true,
+      ),
+    );
+
+    _streamSubscription = FirebaseAuth.instance.authStateChanges().listen(
+      (user) {
+        emit(
+          RootState(
+            user: user,
+            errorMessage: '',
+            isLoading: false,
+          ),
+        );
+      },
+    )..onError(
+        (error) {
+          emit(
+            RootState(
+              user: null,
+              errorMessage: error.toString(),
+              isLoading: false,
+            ),
+          );
+        },
+      );
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
+  }
+}
